442d99f56b1572b220aa5b6897a2c1798ff609f2
Daniil <stepanov0995@mail.ru>
2017-12-19 16:10:10 +0300

    Init

--- target/eos-1.0-SNAPSHOT/resources/js/controller.js
+++
@@ -1,6 +0,0 @@
-app.controller('helloController', function($scope, $http) {
-    $http.get('http://localhost:8080/greeting').
-    then(function(response) {
-        $scope.greeting = response.data;
-    });
-});
